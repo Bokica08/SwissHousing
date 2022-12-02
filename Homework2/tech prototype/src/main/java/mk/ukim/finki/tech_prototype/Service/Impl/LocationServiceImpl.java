b881acf46843b461aaa705ccc99b3f149b572a49
Bojan <bojantrpeski123@gmail.com>
2022-12-02 16:22:05 +0100

    Added REST controllers for the services and made little change in the LocationService.

--- Homework2/tech prototype/src/main/java/mk/ukim/finki/tech_prototype/Service/Impl/LocationServiceImpl.java
+++ Homework2/tech prototype/src/main/java/mk/ukim/finki/tech_prototype/Service/Impl/LocationServiceImpl.java
@@ -2,6 +2,7 @@ package mk.ukim.finki.tech_prototype.Service.Impl;
 
 import mk.ukim.finki.tech_prototype.Model.Exception.InvalidArgumentsException;
 import mk.ukim.finki.tech_prototype.Model.Location;
+import mk.ukim.finki.tech_prototype.Model.Review;
 import mk.ukim.finki.tech_prototype.Repository.LocationRepository;
 import mk.ukim.finki.tech_prototype.Service.LocationService;
 import org.springframework.stereotype.Service;
@@ -31,5 +32,11 @@ public class LocationServiceImpl implements LocationService {
         location.setRating(grades);
         location.setNumOfRatings(num);
         return Optional.of(locationRepository.save(location));
+    }
+
+    @Override
+    public Optional<Location> findById(Long id) {
+
+            return locationRepository.findById(id);
     }
 }
