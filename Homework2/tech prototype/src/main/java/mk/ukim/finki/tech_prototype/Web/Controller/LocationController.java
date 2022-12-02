800ec0a375604c9e724513692832c8feee0f20dd
Bojan <bojantrpeski123@gmail.com>
2022-12-02 17:18:53 +0100

    Making additional changes in LocationController

--- Homework2/tech prototype/src/main/java/mk/ukim/finki/tech_prototype/Web/Controller/LocationController.java
+++ Homework2/tech prototype/src/main/java/mk/ukim/finki/tech_prototype/Web/Controller/LocationController.java
@@ -27,8 +27,8 @@ public class LocationController {
         if(this.locationService.findById(id).isEmpty()) return ResponseEntity.ok().build();
         return ResponseEntity.badRequest().build();
     }
-    @PostMapping("/grade")
-    public Optional<Location> addGrade(@RequestParam Long id,@RequestParam int g)
+    @PostMapping("/grade/{id}")
+    public Optional<Location> addGrade(@PathVariable Long id,@RequestParam int g)
     {
         return locationService.addGrade(id,g);
     }
